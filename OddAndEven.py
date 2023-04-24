392a5a8c0a97951871457d4627295be83b8ed042
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 11:41:09 +0800

    checking the even numbers!

--- OddAndEven.py
+++ OddAndEven.py
@@ -1,12 +1,17 @@
 #Dahan, Regine Fae M. Dahan BSCPE 1-5 File Handling No.1
 
 #open numbers.txt
-with open("numbers.txt") as integers_file, open ("even.txt", "a") as even_file, open("odd.txt", "a") as odd_file:
+with open("numbers.txt", "r") as integers_file, open ("even.txt", "a") as even_file, open("odd.txt", "a") as odd_file:
 #read numbers.txt
     for line in integers_file:
         print(line.strip())
+        
 #if the number is even
+        if integers_file % 2 != 1:
+        
 
 #   write it in even.txt
 #if the number is odd
+        else:
 #   write it in odd.txt
+
