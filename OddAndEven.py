9ea8db2254dce1ef96230384091fc03e3f135deb
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 13:38:32 +0800

    Adding features(Intro): Pyfiglet, Time, Colorama

--- OddAndEven.py
+++ OddAndEven.py
@@ -1,13 +1,34 @@
 #Dahan, Regine Fae M. Dahan BSCPE 1-5 File Handling No.1
 
+#introduction
+import pyfiglet
+import time
+import colorama
+from colorama import Fore
+colorama.init()
+the_intro = 'HELLO'
+print(Fore.CYAN+pyfiglet.figlet_format(the_intro,font="isometric1"))
+time.sleep(1)
+border = "—" * 100
+print(border)
+print(Fore.LIGHTMAGENTA_EX+' This Program is entitled OddAndEven. It will separate the odd and even from twenty integers.')
+print(border)
+time.sleep(2)
+
+print(Fore.LIGHTRED_EX+pyfiglet.figlet_format("THE INTEGERS ARE...",font="digital"))
+time.sleep (2) 
+
 #open numbers.txt
 with open("numbers.txt", "r") as integers_file, open ("even.txt", "a") as even_file, open("odd.txt", "a") as odd_file:
+
+
 
 #read numbers.txt
     for line in integers_file:
         print(line.strip())
         integers_file = int(line)
-        
+
+
 #if the number is even
         if integers_file % 2 != 1:
 
@@ -20,12 +41,20 @@ with open("numbers.txt", "r") as integers_file, open ("even.txt", "a") as even_f
 #   write it in odd.txt
             odd_file.write(str(integers_file) +"\n")
 
+print(border)
+time.sleep(2)
+print(Fore.CYAN+pyfiglet.figlet_format("The Even Integers...",font="digital"))
+time.sleep(2)
 
 #display the even integers
 with open ("even.txt", "r") as even_file:
     for line in even_file:
         print("*"*5)
         print(line.strip())
+
+time.sleep(2)
+print(Fore.GREEN+pyfiglet.figlet_format("The Odd Integers...",font="digital"))
+time.sleep(2)
 
 #display the odd integers
 with open("odd.txt", "r") as odd_file:
