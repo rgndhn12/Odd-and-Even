c5374339734b05ed09924752bd335c575b6d09b0
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 10:22:01 +0800

    the numbers.txt is now reading!

--- OddAndEven.py
+++ OddAndEven.py
@@ -3,6 +3,8 @@
 #open numbers.txt
 with open("numbers.txt") as integers_file:
 #read numbers.txt
+    for line in integers_file:
+        print(line.strip())
 #if the number is even
 #   write it in even.txt
 #if the number is odd
