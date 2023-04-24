521f91e8b44eb1f74dcc6d7c9dd4100b00bdcc0a
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 10:20:15 +0800

    the numbers.txt is now opening!

--- OddAndEven.py
+++ OddAndEven.py
@@ -1,6 +1,7 @@
 #Dahan, Regine Fae M. Dahan BSCPE 1-5 File Handling No.1
 
 #open numbers.txt
+with open("numbers.txt") as integers_file:
 #read numbers.txt
 #if the number is even
 #   write it in even.txt
