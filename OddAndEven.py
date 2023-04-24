79e980b1c66d01e5b1acd7e0066c242b1ef1fa3b
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 10:03:54 +0800

    Pseudocode

---
+++ OddAndEven.py
@@ -0,0 +1,8 @@
+#Dahan, Regine Fae M. Dahan BSCPE 1-5 File Handling No.1
+
+#open numbers.txt
+#read numbers.txt
+#if the number is even
+#   write it in even.txt
+#if the number is odd
+#   write it in odd.txt
