a25682e01f85b619bb89e82d56ba680020b49bf2
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 10:29:41 +0800

    even.txt and odd.txt is now open!

--- OddAndEven.py
+++ OddAndEven.py
@@ -1,11 +1,12 @@
 #Dahan, Regine Fae M. Dahan BSCPE 1-5 File Handling No.1
 
 #open numbers.txt
-with open("numbers.txt") as integers_file:
+with open("numbers.txt") as integers_file, open ("even.txt", "a") as even_file, open("odd.txt", "a") as odd_file:
 #read numbers.txt
     for line in integers_file:
         print(line.strip())
 #if the number is even
+
 #   write it in even.txt
 #if the number is odd
 #   write it in odd.txt
